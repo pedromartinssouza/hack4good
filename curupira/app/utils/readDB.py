27afee0fe3c1490f766ab9fab000bd3bdc61b9f2
Felipe Ricardi <felipe.ricardi@sap.com>
2020-10-24 20:24:26 -0300

    Updating reading database class

--- curupira/app/utils/readDB.py
+++ curupira/app/utils/readDB.py
@@ -3,16 +3,18 @@ from . import utils
 from datetime import datetime, timedelta, timezone
 import random
 import json
-
 def readMonitoring():
 
     localizationQuerySet = Localization.objects.values()
 
     result = list(localizationQuerySet.values())
-    
     for i, s in enumerate(result):
-        result[i]['lat'] = s['lat']/10000
-        result[i]['longit'] = s['longit']/10000
+        parsedLat = s['lat']/10000
+        parsedLongit = s['longit']/10000
+        historicalData = utils.getHistoricalData(parsedLat, parsedLongit, 5)
+        result[i]['lat'] = parsedLat
+        result[i]['longit'] = parsedLongit
+        result[i]['historical'] = historicalData
         result[i]['flag'] = random.choice(['positive', 'careful', 'alert'])
 
     return json.dumps(result)
