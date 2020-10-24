ecc69ad18969cdc1fd43b3f5f9fb2bfe8675b59b
Felipe Ricardi <felipe.ricardi@sap.com>
2020-10-24 20:24:14 -0300

    Updating weather api handling methods

--- curupira/app/utils/utils.py
+++ curupira/app/utils/utils.py
@@ -1,6 +1,7 @@
 from pyowm.owm import OWM
 from pyowm.utils import timestamps
 from datetime import datetime, timedelta, timezone
+import json
 
 owm = OWM('df60f155083559993c09dbf65eda2992')
 
@@ -20,12 +21,41 @@ def getWeatherData(pLat, pLongit, tempUnit):
     
 
 def getHistoricalData(pLat, pLongit, days):
-    date = int((datetime.now() - timedelta(days=days)).replace(tzinfo=timezone.utc).timestamp())
 
     lat = float(pLat)/10000
     longit = float(pLongit)/10000
 
-    forecastHistory = mgr.one_call_history(lat=lat, lon=longit, dt=date)
+    historicalData = list()
 
-    return (forecastHistory.current)
+    for x in range(5):
+        date = int((datetime.now() - timedelta(days=x+1)).replace(tzinfo=timezone.utc).timestamp())
+        history = mgr.one_call_history(lat=lat, lon=longit, dt=date)
+        historicalData.append({
+            'dewpoint': history.current.dewpoint,    
+            'temperature': history.current.temperature(unit="celsius"),
+            'humidity': history.current.humidity,
+            'wind': history.current.wind(unit="meters_sec"),
+            'date': date
+        })
+        
 
+    return (historicalData)
+
+def getCurrentCompleteWeatherData(pLat, pLongit):
+    lat = float(pLat)/10000
+    longit = float(pLongit)/10000
+    date = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+
+    history = mgr.one_call(lat=lat, lon=longit)
+    return {
+        'dewpoint': history.current.dewpoint,    
+        'temperature': history.current.temperature(unit="celsius"),
+        'humidity': history.current.humidity,
+        'wind': history.current.wind(unit="meters_sec"),
+        'date': date,
+        'pressure': history.current.pressure
+    }
+
+class forecast:  
+    def __init__(self, temperature):  
+        self.temperature = temperature  
